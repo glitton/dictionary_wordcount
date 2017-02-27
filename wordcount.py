1d8ac368cccadd3bcc843ca936e864e16dec9e9e
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-27 12:48:16 -0800

    finalized wordcount.py file.

--- wordcount.py
+++ wordcount.py
@@ -1,12 +1,16 @@
-the_file = open('test.txt')
-word_counts = {}
+def word_count(file_name):
+    the_file = open(file_name)
+    word_counts = {}
 
-for line in the_file:
+    for line in the_file:
 
-    line_split = line.strip().split(' ')
-    for word in line_split:
-        word_counts[word] = word_counts.get(word, 0) + 1
+        line_split = line.strip().split()
+        for word in line_split:
+            word_counts[word] = word_counts.get(word, 0) + 1
+        
+    the_file.close()
 
-the_file.close()
+    for word, number in word_counts.items():
+        print word, number
 
-print word_counts
+word_count('twain.txt')
